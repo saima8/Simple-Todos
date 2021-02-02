500f3400c7f8ac012c1117730036def3443d6d6f
Saima Khan <tithi888khan@gmail.com>
2021-02-02 09:30:50 +0600

    Added functionalities of update and remove tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,6 +4,16 @@ import {useTracker} from 'meteor/react-meteor-data';
 import {TasksCollection} from '../api/TasksCollection';
 import {TaskForm} from './TaskForm';
 
+const togglechecked =({_id, ischecked}) => {
+    TasksCollection.update(_id, {
+        $set: {
+            ischecked: !ischecked
+        }
+    })
+};
+
+const deleteTask = ({_id}) => TasksCollection.remove(_id);
+
 export const App = () => {
     const tasks = useTracker(() => TasksCollection.find({}, {sort: {createdAt: -1} }).fetch());
 }
@@ -15,6 +25,8 @@ return (
         <TaskForm />
         <ul>
             {tasks.map(task => <Task key={task_id} task={task}/>)}
+            onCheckboxClick={togglechecked}
+            onDeleteClick={deleteTask}
         </ul>
     
     </div>
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export const Task = ({task}) => {
-    return <li>{task.text}</li>
+export const Task = ({task, onCheckboxClick, onDeleteClick}) => {
+    return (
+        <li>
+            <input type="checkbox"
+                checked={!task.ischecked}
+                onClick={() => onCheckboxClick(task)}
+                readOnly
+            />
+            <span>{task.text}</span>
+            <button onClick={() => onDeleteClick(task)}>&times!</button>
+        </li>
+    )
 };
