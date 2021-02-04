7a841620dc05271b70cc5cf57af3052ccd8719aa
Saima Khan <tithi888khan@gmail.com>
2021-02-04 23:35:59 +0600

    Adding optimistic UI

---
+++ imports/api/taskMethods.js
@@ -0,0 +1,41 @@
+import {check} from 'meteor/check'
+import {TasksCollection} from './TasksCollection'
+
+Meteor.methods({
+    'tasks.insert'(text){
+        check(text, String);
+
+        if(!this.userID){
+            throw new Meteor.Error ('Not authorized');
+
+        }
+        TasksCollection.insert({
+            text,
+            createdAt: new Date,
+            userID: this.userID,
+        })
+    },
+    'tasks.remove'(taskId){
+        check(taskId, String);
+
+        if (!this.userID){
+            throw new Meteor.Error('Not authorized.');
+        }
+        TasksCollection.remove(taskId);
+
+    },
+    'tasks.setIsChecked'(taskId, isChecked) {
+        check(taskId, String);
+        check(isChecked, Boolean);
+     
+        if (!this.userId) {
+          throw new Meteor.Error('Not authorized.');
+        }
+     
+        TasksCollection.update(taskId, {
+          $set: {
+            isChecked
+          }
+        });
+      }
+})
